293ae1b6c304fd96226ecb6380338a4dc97aa333
Sascha Vincent Kurowski <svkurowski@gmail.com>
2019-02-02 10:23:01 +0100

    Fixes bug where acted_at was datetime

--- app/models/document.rb
+++ app/models/document.rb
@@ -5,11 +5,11 @@ class Document < ApplicationRecord
   has_one_attached :original_file
 
   def acted_at
-    received_at || sent_at || created_at
+    received_at || sent_at || created_at.to_date
   end
 
   def self.order_by_acted_at(dir = 'DESC')
     dir = if ['ASC', 'DESC'].include? dir then dir else 'DESC' end
-    order("COALESCE(received_at, sent_at, created_at) #{dir}")
+    order("COALESCE(received_at, sent_at, created_at::date) #{dir}")
   end
 end
