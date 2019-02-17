c8873a1903e54dde3c8693b43dd6f9af8f320d75
markokajzer <markokajzer91@gmail.com>
2019-02-17 22:12:37 +0100

    Applies code style of project

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,3 @@
 class ApplicationController < ActionController::Base
-    http_basic_authenticate_with Rails.configuration.authentication_options
+  http_basic_authenticate_with Rails.configuration.authentication_options
 end
