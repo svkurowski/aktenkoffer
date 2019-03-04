63f7b5a6526d45718dfbc32336ff8e3edeaf12a6
markokajzer <markokajzer91@gmail.com>
2019-03-04 18:47:58 +0100

    Make all methods static

--- lib/search/document_search.rb
+++ lib/search/document_search.rb
@@ -1,19 +1,10 @@
 module Search
   class DocumentSearch
-    def self.perform(query)
-      new(query).send(:perform)
-    end
-
-    def initialize(query)
-      @query = query
-    end
-
-    private
-
-      def perform
+    class << self
+      def perform(query)
         documents = Document.all
 
-        params, title_queries = extract_search_params_from(@query)
+        params, title_queries = extract_search_params_from(query)
         return documents unless params.any? || (title_queries.present? && title_queries.any?)
 
         documents = filter_attributes(documents, title_queries)
@@ -22,30 +13,33 @@ module Search
         documents
       end
 
-      def extract_search_params_from(query)
-        return {} unless query.present?
+      private
 
-        query
-          .split(' ')
-          .map { |param| param.split(':') }
-          .map { |(key, value)| { key => value } }
-          .reduce(&:merge)
-          .partition { |(_, value)| value.present? }
-          .map(&:to_h)
-      end
+        def extract_search_params_from(query)
+          return {} unless query.present?
 
-      def filter_attributes(documents, params)
-        params.keys.reduce(documents) do |result, attribute|
-          query_method, filter_value = filter_query_for(params, attribute)
-          result = result.send(query_method, filter_value) if result.respond_to?(query_method)
-          result
+          query
+            .split(' ')
+            .map { |param| param.split(':') }
+            .map { |(key, value)| { key => value } }
+            .reduce(&:merge)
+            .partition { |(_, value)| value.present? }
+            .map(&:to_h)
         end
-      end
 
-      def filter_query_for(params, attribute)
-        return ["with_#{attribute}", params[attribute]] if params[attribute].present?
+        def filter_attributes(documents, params)
+          params.keys.reduce(documents) do |result, attribute|
+            query_method, filter_value = filter_query_for(params, attribute)
+            result = result.send(query_method, filter_value) if result.respond_to?(query_method)
+            result
+          end
+        end
 
-        ['with_title', attribute]
-      end
+        def filter_query_for(params, attribute)
+          return ["with_#{attribute}", params[attribute]] if params[attribute].present?
+
+          ['with_title', attribute]
+        end
+    end
   end
 end
