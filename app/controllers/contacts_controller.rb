6bcfbbb1a73fc16b187a35d9c2268b70999f0d57
markokajzer <markokajzer91@gmail.com>
2019-03-08 23:01:03 +0100

    Styled contact forms

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -44,7 +44,7 @@ class ContactsController < ApplicationController
   def destroy
     @contact.destroy
     respond_to do |format|
-      format.html { redirect_to contacts_url, notice: 'Contact was successfully destroyed.' }
+      format.html { redirect_to contacts_url, notice: 'Contact was successfully deleted.' }
       format.json { head :no_content }
     end
   end
