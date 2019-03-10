139ab8386021d3c72d36173004c2c34689ec1fa5
markokajzer <markokajzer91@gmail.com>
2019-03-10 10:20:01 +0100

    Refactored PaginationHelper

--- app/helpers/pagination_helper.rb
+++ app/helpers/pagination_helper.rb
@@ -7,13 +7,7 @@ module PaginationHelper
       page_prev = pages.delete(:previous_page)
       page_next = pages.delete(:next_page)
 
-      list_items = pages.map { |item| item.is_a?(Integer) ? page_number(item) : send(item) }
-                        .join(@options[:link_separator])
-
-      content = tag('ul', list_items, class: 'pagination-list')
-      content.prepend(next_page) if page_next
-      content.prepend(previous_page) if page_prev
-
+      content = content_for_pagination(pages, page_prev, page_next)
       tag('nav', content, class: "pagination is-centered #{@options[:class]}")
     end
 
@@ -30,16 +24,22 @@ module PaginationHelper
         tag :li, link(page, page, link_options.merge(rel: rel_value(page), class: 'pagination-link'))
       end
 
-      def previous_or_next_page(page, text, classname)
-        link_options = @options[:link_options] || {}
-
-        return tag(:li, link(text, page, link_options), class: classname) if page
-
-        tag :li, tag(:span, text), class: classname, disabled: true
-      end
-
       def gap
         tag :li, '<span class="pagination-ellipsis">&hellip;</span>'
+      end
+
+      def content_for_pagination(pages, page_prev, page_next)
+        list_items = list_items_for(pages)
+
+        content = tag('ul', list_items, class: 'pagination-list')
+        content.prepend(next_page) if page_next
+        content.prepend(previous_page) if page_prev
+        content
+      end
+
+      def list_items_for(pages)
+        pages.map { |item| item.is_a?(Integer) ? page_number(item) : send(item) }
+             .join(@options[:link_separator])
       end
 
       def previous_page
@@ -52,6 +52,14 @@ module PaginationHelper
         num = @collection.current_page + 1 if @collection.current_page < @collection.total_pages
 
         previous_or_next_page(num, @options[:next_label], 'pagination-next')
+      end
+
+      def previous_or_next_page(page, text, classname)
+        link_options = @options[:link_options] || {}
+
+        return tag(:li, link(text, page, link_options), class: classname) if page
+
+        tag :li, tag(:span, text), class: classname, disabled: true
       end
   end
 end
