73b76be45187e20205f732275d09aeb6e34c0c5f
Sascha Vincent Kurowski <svkurowski@gmail.com>
2019-04-19 11:48:49 +0200

    Removes basic authentication (#7)
    The basic authentication has been replaced with user authentication using Devise.
    
    This reverts commit dd66ba4893531342d7ac740548375d9762941525.
    
    Closes #7.

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -2,7 +2,6 @@ module API
   class BaseController < ActionController::API
     include ActionController::HttpAuthentication::Basic::ControllerMethods
 
-    http_basic_authenticate_with Rails.configuration.authentication_options
     before_action :authenticate_user!
   end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-  http_basic_authenticate_with Rails.configuration.authentication_options
-
   before_action :authenticate_user!
 end
