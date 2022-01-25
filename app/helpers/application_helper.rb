05786bd32b52a230085ab6c44d293c87e115991d
Sascha Vincent Kurowski <svkurowski@gmail.com>
2022-01-25 05:24:37 +0100

    Adds home contact configuration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def home_contact
+    home_contact_id = Aktenkoffer::Application.config.home_contact_id
+    if home_contact_id.nil?
+      nil
+    else
+      Contact.find(Aktenkoffer::Application.config.home_contact_id)
+    end
+  end
 end
