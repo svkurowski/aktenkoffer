0f1a4c41f1597fb76e28d9f197f1c3abfb6334e4
Sascha Vincent Kurowski <svkurowski@gmail.com>
2022-02-04 11:00:20 +0100

    Fixes error in document show and edit pages for Safari

--- app/helpers/active_storage_url_helper.rb
+++ app/helpers/active_storage_url_helper.rb
@@ -2,7 +2,7 @@ module ActiveStorageUrlHelper
   def object_url_for(blob)
     if browser.safari?
       ActiveStorage::Current.set(host: request.base_url) do
-        blob.service_url
+        blob.url
       end
     else
       url_for blob
