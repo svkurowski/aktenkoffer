ce4704f4c75a3039b2b8c7fa17775189842edfef
Sascha Vincent Kurowski <svkurowski@gmail.com>
2022-02-20 18:30:16 +0100

    Converts Navbar handler to Stimulus controller

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -7,15 +7,16 @@ import './components/ErrorNotification';
 import './components/Notification';
 import './components/WarningNotification';
 import './elements/modals';
-import './elements/navBar';
 import './elements/prepareFileDropOverlay';
 import './elements/prepareFileInputs';
 import './elements/tagInputs';
 import './pages/documents';
+import NavBarController from './controllers/nav_bar_controller';
 import SelectContactController from './controllers/select_contact_controller';
 
 (() => {
   ActiveStorage.start();
   window.Stimulus = Application.start();
+  window.Stimulus.register('nav-bar', NavBarController);
   window.Stimulus.register('select-contact', SelectContactController);
 })();
