f7f7bb99d66889b7b81899935673b3040a8a95a3
Sascha Vincent Kurowski <svkurowski@gmail.com>
2022-02-25 10:04:37 +0100

    Introduces system tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,12 +1,18 @@
-ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
-require 'rails/test_help'
+ENV["RAILS_ENV"] ||= "test"
+require_relative "../config/environment"
+require "rails/test_help"
 
-class ActiveSupport
-  module TestCase
-    # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-    fixtures :all
+class ActiveSupport::TestCase
+  # Run tests in parallel with specified workers
+  parallelize(workers: :number_of_processors)
 
-    # Add more helper methods to be used by all tests here...
+  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
+  fixtures :all
+
+  # Add more helper methods to be used by all tests here...
+  include Warden::Test::Helpers
+
+  def login
+    login_as(users(:one), scope: :user)
   end
 end
