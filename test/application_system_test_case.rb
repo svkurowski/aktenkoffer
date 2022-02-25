6e0dc39f6c1d0ce3d38a1164bf340f73075c9b2b
Sascha Kurowski <skkurows@amazon.com>
2022-02-25 10:13:17 +0100

    Runs system tests headless by default

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  DRIVER = ['0', 'false'].include?(ENV["HEADLESS"]&.downcase) ? :chrome : :headless_chrome
+
+  driven_by :selenium, using: DRIVER, screen_size: [1400, 1400]
 end
